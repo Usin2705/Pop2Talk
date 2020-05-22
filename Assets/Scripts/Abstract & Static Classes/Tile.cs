00f0dc04111abea094ddeb1a9ccd88c572f11d7a
LoHii <vertti.viitanen@gmail.com>
2020-05-22 13:20:55 +0300

    - Game doesn't freeze if a moving matchable tile is clicked

--- Assets/Scripts/Abstract & Static Classes/Tile.cs
+++ Assets/Scripts/Abstract & Static Classes/Tile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [SelectionBase]
 public abstract class Tile : Interactable {
 
-    /*public override bool Interact(InteractType type) {
+	/*public override bool Interact(InteractType type) {
         if (!base.Interact(type))
             return false;
 
@@ -15,69 +15,73 @@ public abstract class Tile : Interactable {
             return false;
     }*/
 
-   protected bool alreadyPopped;
-   protected Vector3 childStart;
+	protected bool alreadyPopped;
+	protected Vector3 childStart;
+	protected bool moving;
 
-   public virtual bool CanPop {
-        get {
-            return !alreadyPopped;
-        }
-    }
+	public virtual bool CanPop {
+		get {
+			return !alreadyPopped;
+		}
+	}
 
-    public virtual void PopVisual(float duration) {
+	public virtual void PopVisual(float duration) {
 
-    }
+	}
 
-    public virtual void GrowVisual(float duration) {
+	public virtual void GrowVisual(float duration) {
 
-    }
-    
-    public virtual void ClickVisual(float duration) {
+	}
 
-    }
+	public virtual void ClickVisual(float duration) {
 
-    public virtual void MoveToVisual(Vector3 position, float duration) {
+	}
 
-    }
+	public virtual void MoveToVisual(Vector3 position, float duration) {
 
-    protected virtual void Awake() {
-        childStart = transform.GetChild(0).localPosition;
-    }
+	}
 
-    protected IEnumerator ScaleAndVibrate(float duration, float startScale, float targetScale, float delay = 0) {
-        if (delay > 0)
-            yield return new WaitForSeconds(delay);
-        if (duration <= 0)
-            yield break;
-        float a = 0;
-        Vector3 startPos = transform.GetChild(0).localPosition;
-        Vector3 baseScale = transform.localScale;
-        float vibrationAmount = 0.1f;
-        while (a < 1) {
-            a += Time.deltaTime / duration;
-            transform.GetChild(0).localPosition = startPos + new Vector3(vibrationAmount * Random.Range(-1f, 1f), vibrationAmount * Random.Range(-1f, 1f));
-            transform.localScale = Vector3.Lerp(baseScale * startScale, baseScale * targetScale, a * a);
-            yield return null;
-        }
-        transform.GetChild(0).localPosition = Vector3.zero;
-    }
+	protected virtual void Awake() {
+		childStart = transform.GetChild(0).localPosition;
+	}
 
-    protected IEnumerator MoveTowards(Vector3 targetPosition, float duration) {
-        Vector3 startPosition = transform.position;
-        float a = 0;
-        while (a < 1) {
-            a += Time.deltaTime/duration;
-            transform.position = Vector3.LerpUnclamped(startPosition, targetPosition, a) - Vector3.forward; // -Vector3 forward so that moving tile is on top
-            yield return null;
-        }
-    }
+	protected IEnumerator ScaleAndVibrate(float duration, float startScale, float targetScale, float delay = 0) {
+		if (delay > 0)
+			yield return new WaitForSeconds(delay);
+		if (duration <= 0)
+			yield break;
+		float a = 0;
+		Vector3 startPos = transform.GetChild(0).localPosition;
+		Vector3 baseScale = transform.localScale;
+		float vibrationAmount = 0.1f;
+		while (a < 1) {
+			a += Time.deltaTime / duration;
+			transform.GetChild(0).localPosition = startPos + new Vector3(vibrationAmount * Random.Range(-1f, 1f), vibrationAmount * Random.Range(-1f, 1f));
+			transform.localScale = Vector3.Lerp(baseScale * startScale, baseScale * targetScale, a * a);
+			yield return null;
+		}
+		transform.GetChild(0).localPosition = Vector3.zero;
+	}
 
-    public virtual void Reset() {
-        alreadyPopped = false;
-        transform.GetChild(0).localPosition = childStart;
-    }
+	protected IEnumerator MoveTowards(Vector3 targetPosition, float duration) {
+		Vector3 startPosition = transform.position;
+		moving = true;
+		float a = 0;
+		while (a < 1) {
+			a += Time.deltaTime / duration;
+			transform.position = Vector3.LerpUnclamped(startPosition, targetPosition, a) - Vector3.forward; // -Vector3 forward so that moving tile is on top
+			yield return null;
+		}
+		transform.position = targetPosition;
+		moving = false;
+	}
 
-    public virtual void Pop() {
-        alreadyPopped = true;
-    }
+	public virtual void Reset() {
+		alreadyPopped = false;
+		transform.GetChild(0).localPosition = childStart;
+	}
+
+	public virtual void Pop() {
+		alreadyPopped = true;
+	}
 }
