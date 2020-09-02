29746d216a6526f5962ce0ebe271ae3fdfedddea
LoHii <vertti.viitanen@gmail.com>
2020-09-02 08:15:49 +0300

    - Starting Pairs prototype

---
+++ Assets/Scripts/Game Pieces/PairCard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PairCard : Interactable {
+
+	Sprite frontSprite;
+	Sprite backSprite;
+
+	Callback Click;
+
+	float speed = 0;
+
+	public bool Open {
+		get {
+			return true;
+		}
+	}
+
+	public bool Rotating {
+		get {
+			return true;
+		}
+	}
+
+	void Start() {
+
+	}
+
+	public void SetSprites(Sprite front, Sprite back) {
+		frontSprite = front;
+		backSprite = back;
+		UpdateSprite();
+	}
+
+	void UpdateSprite() {
+
+	}
+
+	public override bool Interact(InteractType type) {
+		if (base.Interact(type)) {
+			Click?.Invoke();
+			return true;
+		}
+		return false;
+	}
+
+	public void Rotate(bool open, Callback Done) {
+
+	}
+
+	public void Move(Vector3 target) {
+		Move(target, speed);
+	}
+
+	public void Move(Vector3 target, float speed) {
+
+	}
+
+	protected override bool ValidInteracting(InteractType type) {
+		return type == InteractType.Press;
+	}
+}
---
+++ Assets/Scripts/Views/PairsView.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PairsView : View {
+
+	[SerializeField] View shipHub;
+	[SerializeField] GameObject pairPrefab;
+	[SerializeField] Sprite backSprite;
+	[SerializeField] float shufflespeed;
+
+	WordData[] words;
+
+	Dictionary<PairCard, WordData> pairs = new Dictionary<PairCard, WordData>();
+
+	protected override void Initialize() {
+		base.Initialize();
+	}
+
+	public override void Activate() {
+		base.Activate();
+	}
+
+	void PrepareCards() {
+		Vector3 startPos = Vector3.zero;
+		Transform cardRoot = null;
+		PairCard pairCard;
+		List<PairCard> pairCards = new List<PairCard>();
+		for (int i = 0; i < words.Length; ++i) {
+			for (int j = 0; i < 2; ++j) {
+				pairCard = Instantiate(pairPrefab, cardRoot).GetComponent<PairCard>();
+				pairCard.transform.position = startPos;
+				pairCard.SetSprites(words[i].picture, backSprite);
+				pairs.Add(pairCard, words[i]);
+			}
+		}
+		StartCoroutine(ArrangePairCards(pairCards));
+	}
+
+	IEnumerator ArrangePairCards(List<PairCard> pairCards) {
+		pairCards.Shuffle();
+		Vector3[] positions = GetCardPositions(pairCards.Count);
+
+		yield return null;
+	}
+
+	Vector3[] GetCardPositions(int count) {
+		return null;
+	}
+	
+	void GotoShipHub() {
+		ViewManager.GetManager().ShowView(shipHub);
+	}
+
+	public override UIButton GetPointedButton() {
+		return null;
+	}
+
+	public override UIButton[] GetAllButtons() {
+		return null;
+	}
+}
